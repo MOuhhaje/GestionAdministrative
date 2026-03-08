40f8d6f5f88ec2d49b21a1e89e3db28570e5da52
Oussama Mouhhaje <78048534+MOuhhaje@users.noreply.github.com>
2026-03-08 09:38:04 +0000

    Update start.sh to include database seeding

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ sed -i "s/:80/:${PORT:-80}/" /etc/apache2/sites-available/*.conf
 
 php artisan config:clear
 php artisan storage:link --force
-php artisan migrate --force --no-interaction 2>/dev/null || true
+php artisan migrate --force --no-interaction
+php artisan db:seed --force --no-interaction 2>/dev/null || true
 
 apache2-foreground
