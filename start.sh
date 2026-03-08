45b798d395771b8c741a2f592632f855b98c0547
Oussama Mouhhaje <78048534+MOuhhaje@users.noreply.github.com>
2026-03-08 09:34:58 +0000

    Modify migration command in start.sh
    Updated migration command to suppress output and avoid errors.

--- start.sh
+++ start.sh
@@ -5,14 +5,8 @@ echo "ServerName localhost" >> /etc/apache2/apache2.conf
 sed -i "s/Listen 80/Listen ${PORT:-80}/" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT:-80}/" /etc/apache2/sites-available/*.conf
 
-# Show what env vars are available
-echo "APP_KEY: $APP_KEY"
-echo "DB_CONNECTION: $DB_CONNECTION"
-echo "DB_HOST: $DB_HOST"
-
 php artisan config:clear
-php artisan key:generate --force
 php artisan storage:link --force
-php artisan migrate --force
+php artisan migrate --force --no-interaction 2>/dev/null || true
 
 apache2-foreground
